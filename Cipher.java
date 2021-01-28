257d61645f2132d138dc7f0fe1c624357458d87c
ndtu <77379218+ndtu@users.noreply.github.com>
2021-01-27 23:31:30 -0800

    Update Cipher.java

--- Cipher.java
+++ Cipher.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Cipher {
 
-    public static boolean isLowerCase(char letter) {
+    private static boolean isLowerCase(char letter) {
         Scanner next1 = new Scanner(System.in);
         char first = next1.nextLine().toCharArray()[0];
         boolean second = Character.isLowerCase(first);
@@ -14,7 +14,7 @@ public class Cipher {
     
 } 
 }
-    public static boolean isLowerCase(String str) {
+    private static boolean isLowerCase(String str) {
         Scanner next2 = new Scanner(System.in);
         String third = next2.nextLine();
         char[] CharacterArray = third.toCharArray();
