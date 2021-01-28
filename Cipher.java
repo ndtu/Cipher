e125f4539df96150b0ea4223df48f71b0d255a61
ndtu <77379218+ndtu@users.noreply.github.com>
2021-01-27 18:24:33 -0800

    Update and rename README.md to Cipher.java

---
+++ Cipher.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Cipher {
+
+    public static boolean isLowerCase(char letter) {
+        Scanner next1 = new Scanner(System.in);
+        char first = next1.nextLine().toCharArray()[0];
+        boolean second = Character.isLowerCase(first);
+        if(second) {
+            return true;
+        }
+        else{
+            return false;
+    
+} 
+}
+    public static boolean isLowerCase(String str) {
+        return true;
+    }
+}
